b50058c5be6e9dc02e5e6eb04df4c229091bdefa
Mirrayy <miraymutlu_@hotmail.com>
2023-11-24 16:05:44 +0300

    1_2tamamlandı

--- Labex2_Q1_tests_210401039_miray_mutlu.R
+++ Labex2_Q1_tests_210401039_miray_mutlu.R
@@ -4,5 +4,8 @@ library(testthat)
 testthat::test_that("Global Workspace'te spotify_token adlı değişken olmalı", {
   expect_true(exists("spotify_token", envir = .GlobalEnv))
 })
-
+#Test 1.2) 
+testthat::test_that("spotify_token adl değişkenin tipi 'function' olmalı", {
+  expect_true(is.function(spotify_token))
+})
 
--- Labex2_Q2_tests_210401039_miray.R
+++ Labex2_Q2_tests_210401039_miray.R
@@ -2,7 +2,7 @@ library(testthat)
 
 current_dir <- getwd()
 print(current_dir)
-relative_path <- file.path(current_dir, "Labex2_Q1_210401039_miraymutlu.R")
+relative_path <- file.path(current_dir, "Labex2_Q1_210401039_miray_mutlu.R")
 
 source(relative_path)
 
@@ -11,5 +11,8 @@ source(relative_path)
 test_that("Global Workspace'de spotify_search_artist adlı bir değişken olmalı.", {
   expect_true(exists("spotify_search_artist"))
 })
-
+# Test 2.2: spotify_search_artist adlı değişkenin tipi function olmalı.
+test_that("spotify_search_artist adlı değişkenin tipi 'function' olmalı.", {
+  expect_is(spotify_search_artist, "function")
+})
 
